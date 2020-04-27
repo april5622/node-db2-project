26598fc79838bdf91b10919ac96a1ef2c872faa9
April Ma <54913304+april5622@users.noreply.github.com>
2020-04-26 18:06:52 -0700

    MVP done

---
+++ migrations/20200426175740_transmission-status.js
@@ -0,0 +1,14 @@
+
+exports.up = async function(knex) {
+  await knex.schema.alterTable("cars", (table) => {
+      table.text("transmission type")
+      table.text("status")
+  })
+};
+
+exports.down = async function(knex) {
+  await knex.schema.alterTable("cars", (table) =>{
+      table.dropColumn("transmission type")
+      table.dropColumn("status")
+  })
+};
